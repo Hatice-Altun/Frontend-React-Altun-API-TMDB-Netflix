1435f05bdeb9a67df30305875346390e8b61ab74
Hatice-Altun <van.altun@outlook.com>
2023-07-08 12:39:00 +0200

    Netflix

---
+++ src/HomeScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+
+function HomeScreen() {
+    return(
+      <div className="homescreen">
+        <Nav/>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+
+      </div>
+    );
+}
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBrpLdm_kEAZYUXhr8MMKaTW1V1xLjL85g",
+    authDomain: "netflix-adbf4.firebaseapp.com",
+    projectId: "netflix-adbf4",
+    storageBucket: "netflix-adbf4.appspot.com",
+    messagingSenderId: "816200352172",
+    appId: "1:816200352172:web:c4c4a9675666b78081ba09"
+};
+
+const app = initializeApp(firebaseConfig);
